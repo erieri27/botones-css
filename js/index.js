cd8259f221500a790522c3f6efb015bb3f184c72
erika <erikagp_73@hotmail.com>
2019-01-30 19:56:15 -0600

    ejercicio flex

---
+++ js/index.js
@@ -0,0 +1,13 @@
+//alert("HELLO WORD. MY NAME IS")
+
+function mostrar(){
+    alert('NO HAY NADA');
+}
+const palabraUno = "ana";
+
+const palabraDos = "ana";
+return true;
+
+const palindromo = 
+
+palabra.split.reverse
